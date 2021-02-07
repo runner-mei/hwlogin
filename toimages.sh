4a6b3a46805d56e1875f497f842c2f630964e3cc
meifakun <runner.mei@gmail.com>
2021-02-07 20:18:58 +0800

    update toimages.sh

--- toimages.sh
+++ toimages.sh
@@ -1,4 +1,4 @@
 # ffmpeg.exe -f gdigrab -framerate 15 -offset_x 10 -offset_y 20 -i desktop -r 25  -q:v 2 -vframes 200 -y -f image2 images/image-%%d.jpg >a.log 2>&1
 # @rem ffmpeg -f dshow -i video="USB2.0 VGA UVC WebCam" -r 1  -q:v 2 -vframes 1 -y -f image2 images/image-1.jpg  >a.log 2>&1
 # @rem ffmpeg -t 200 -f vfwcap -i 0 -r 25 -f -y -f image2 images/image-%%d.jpg  >a.log 2>&1
-ffmpeg  -f v4l2 -r 25 -i /dev/video0 -vframes 200 -s 640x480 -f image2 images/image-%d.jpg
+./ffmpeg/bin/ffmpeg  -f v4l2 -r 25 -i /dev/video0 -vframes 200 -s 640x480 -f image2 images/image-%d.jpg
